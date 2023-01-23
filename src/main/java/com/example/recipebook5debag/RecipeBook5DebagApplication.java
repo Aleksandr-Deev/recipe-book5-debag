cd48974e702848606557bf2b7a30f153f9bb1834
Aleksandr Deev <geshtait@mail.ru>
2023-01-23 23:20:27 +0500

    Initial commit

---
+++ src/main/java/com/example/recipebook5debag/RecipeBook5DebagApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebook5debag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBook5DebagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBook5DebagApplication.class, args);
+    }
+
+}
